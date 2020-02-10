75b34ad738cee2d2f5e59f281f1430eb396c3657
Alois <aloiscarrera@gmail.com>
2020-02-10 15:07:34 -0500

    Getting data from search bar

--- src/components/search.js
+++ src/components/search.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 
 export default class search extends Component {
+    
+    get_data=React.createRef();
+
+    passing_data= (e)=>{
+        e.preventDefault();
+        console.log(this.get_data.current.value);
+    }
+    
     render() {
         return (
-            <form>
+            <form onSubmit={this.passing_data}>
                 <div className="row">
                     <div className="col-md-8">
-                        <input type="text" className="form-control form-control-lg" placeholder="Search for an image. Example: Soccer..."/>
+                        <input ref={this.get_data} type="text" className="form-control form-control-lg" placeholder="Search for an image. Example: Soccer..."/>
                     </div>
                     <div className="col-md-4">
                         <input type="submit" className="btn btn-primary btn-lg btn-block"/>
