597129d2dcbb007fbc0ededd1eb77dd5c803911e
Alois <aloiscarrera@gmail.com>
2020-02-11 12:14:50 -0500

    Image result card changes

--- src/components/image.js
+++ src/components/image.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 const Image = (props) => {
 
-    const { downloads, likes, previewURL, userImageURL, tags, comments } = props.image;
+    const { downloads, likes, previewURL, largeImageURL, tags, comments } = props.image;
 
     return (
         <div className="col-12 col-sm-6 col-md-3 col-lg-2 mb-3">
             <div className="card">
                 <img src={previewURL} className="card-img-top" alt={tags} />
-                <div className="card-body">
+                <div className="card-body p-2">
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item"><i className="fab fa-gratipay" style={{marginRight: 20 + 'px'}}></i>{likes}</li>
                         <li className="list-group-item"><i className="fas fa-download" style={{marginRight: 20 + 'px'}}></i>{downloads}</li>
                         <li className="list-group-item"><i className="fas fa-comments" style={{marginRight: 20 + 'px'}}></i>{comments}</li>
+                        <li className="list-group-item pt-2 p-0"><a href={largeImageURL} rel="noopener noreferrer" target="_blank" className="btn btn-primary btn-block">View picture</a></li>
                     </ul>
                 </div>
             </div>
