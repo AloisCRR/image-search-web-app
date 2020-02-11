5b733055c44f4d033cc7e13952368e472cc60819
Alois <aloiscarrera@gmail.com>
2020-02-11 08:42:03 -0500

    Passing images array by props to results.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 // Importing components
 import Search from './components/search'
+import Results from './components/results'
 
 /* Pixabay
 User: kahagi2080@mailmink.com
@@ -11,27 +12,28 @@ Pass: hola1234 */
 function App() {
 
   const [searchWord, setSearchWord] = useState(null);
-  const [images, setImages]=useState([]);
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     if (searchWord !== null) {
-    const url = `https://pixabay.com/api/?key=15216018-e65acb1671acb3c799dd2acbc&q=${searchWord}`;
-    fetch(url)
-      .then(ans=>ans.json())
-      .then(res=>setImages(res.hits))
-  }
-}, [searchWord]);
+      const url = `https://pixabay.com/api/?key=15216018-e65acb1671acb3c799dd2acbc&q=${searchWord}`;
+      fetch(url)
+        .then(ans => ans.json())
+        .then(res => setImages(res.hits))
+    }
+  }, [searchWord]);
 
-return (
-  <div className="container">
-    <div className="jumbotron mt-5">
-      <h3 className="text-center mb-5">Image Search</h3>
-      <Search data={data => {
-        setSearchWord(data);
-      }} />
+  return (
+    <div className="container">
+      <div className="jumbotron mt-5">
+        <h3 className="text-center mb-5">Image Search</h3>
+        <Search data={data => {
+          setSearchWord(data);
+        }} />
+      </div>
+      <Results imagesResults={images}/>
     </div>
-  </div>
-);
+  );
 }
 
 export default App;
--- src/components/results.js
+++ src/components/results.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 
 export default class results extends Component {
+    
+    showImages =()=>{
+        if (this.props.imagesResults.length===0) return null;
+        console.log(this.props.imagesResults);
+    }
+    
     render() {
         return (
-            <div>
-                <p>Hello</p>
-            </div>
+            <React.Fragment>
+                {this.showImages()}
+            </React.Fragment>
         )
     }
 }
