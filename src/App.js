f4be5636c4cbb476749e5c0bc29a0109fc51745b
Alois <aloiscarrera@gmail.com>
2020-02-10 22:01:57 -0500

    Set state with hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 // Importing components
 import Search from './components/search'
 
+/* Pixabay
+User: kahagi2080@mailmink.com
+Pass: hola1234 */
 
 function App() {
+
+  const [searchWord, setSearchWord]= useState("Cafe");
+
   return (
     <div className="container">
-      <div className="jumbotron">
+      <div className="jumbotron mt-5">
         <h3 className="text-center mb-5">Image Search</h3>
         <Search data={(data)=>{
-          console.log(data);
+          setSearchWord(data);
         }}/>
       </div>
+      {searchWord}
     </div>
   );
 }
