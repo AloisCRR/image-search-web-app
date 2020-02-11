4c3794b5f08a97dd1f82665cf1010a0a0e446c5f
Alois <aloiscarrera@gmail.com>
2020-02-11 00:35:29 -0500

    Getting a custom url to API, from search.js component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 // Importing components
@@ -10,19 +10,26 @@ Pass: hola1234 */
 
 function App() {
 
-  const [searchWord, setSearchWord]= useState("Cafe");
+  const [searchWord, setSearchWord] = useState(null);
 
-  return (
-    <div className="container">
-      <div className="jumbotron mt-5">
-        <h3 className="text-center mb-5">Image Search</h3>
-        <Search data={(data)=>{
-          setSearchWord(data);
-        }}/>
-      </div>
-      {searchWord}
+  useEffect(() => {
+    if (searchWord !== null) {
+    const url = `https://pixabay.com/api/?key=15216018-e65acb1671acb3c799dd2acbc&q=${searchWord}`;
+    console.log(url);
+  }
+}, [searchWord]);
+
+return (
+  <div className="container">
+    <div className="jumbotron mt-5">
+      <h3 className="text-center mb-5">Image Search</h3>
+      <Search data={data => {
+        setSearchWord(data);
+      }} />
     </div>
-  );
+    {searchWord}
+  </div>
+);
 }
 
 export default App;
