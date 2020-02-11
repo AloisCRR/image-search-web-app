a70040e0e3994ddc7f772085269bacae6a2627fc
Alois <aloiscarrera@gmail.com>
2020-02-11 08:40:20 -0500

    Component to show search results

---
+++ src/components/results.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class results extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello</p>
+            </div>
+        )
+    }
+}
