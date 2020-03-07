26f05512e72223753ec3e215e3da6c56b79a9b62
Alois Carrera <aloiscarrera@gmail.com>
2020-03-06 21:44:53 -0500

    Pagination fixes

--- src/components/image.js
+++ src/components/image.js
@@ -10,9 +10,9 @@ const Image = (props) => {
                 <img src={previewURL} className="card-img-top" alt={tags} />
                 <div className="card-body p-2">
                     <ul className="list-group list-group-flush">
-                        <li className="list-group-item"><i className="fab fa-gratipay" style={{marginRight: 20 + 'px'}}></i>{likes}</li>
-                        <li className="list-group-item"><i className="fas fa-download" style={{marginRight: 20 + 'px'}}></i>{downloads}</li>
-                        <li className="list-group-item"><i className="fas fa-comments" style={{marginRight: 20 + 'px'}}></i>{comments}</li>
+                        <li className="list-group-item"><i className="fab fa-gratipay" style={{ marginRight: 20 + 'px' }}></i>{likes}</li>
+                        <li className="list-group-item"><i className="fas fa-download" style={{ marginRight: 20 + 'px' }}></i>{downloads}</li>
+                        <li className="list-group-item"><i className="fas fa-comments" style={{ marginRight: 20 + 'px' }}></i>{comments}</li>
                         <li className="list-group-item pt-2 p-0"><a href={largeImageURL} rel="noopener noreferrer" target="_blank" className="btn btn-primary btn-block">View picture</a></li>
                     </ul>
                 </div>
--- src/components/pagination.js
+++ src/components/pagination.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Pagination = props =>{
     return (
         <div className="row justify-content-center py-5">
-            <button type="button" className="btn btn-info btn-lg mr-1"><i className="fas fa-arrow-left" style={{ marginRight: 20 + 'px' }}></i>Anterior</button>
-            <button type="button" className="btn btn-info btn-lg mr-1">Siguiente<i className="fas fa-arrow-right" style={{ marginLeft: 20 + 'px' }}></i></button>
+            <button onClick={props.previousPage} type="button" className="btn btn-info btn-lg mr-1"><i className="fas fa-arrow-left" style={{ marginRight: 20 + 'px' }}></i>Anterior</button>
+            <button onClick={props.nextPage} type="button" className="btn btn-info btn-lg mr-1">Siguiente<i className="fas fa-arrow-right" style={{ marginLeft: 20 + 'px' }}></i></button>
         </div>
     )
 }
