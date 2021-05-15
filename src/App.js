e575603b2e285fadd2aad7853ff3e33bcafc36a6
Alois Carrera <aloiscarrera@gmail.com>
2021-05-15 18:59:33 -0500

    refactor: :fire: Remove old API key

--- src/App.js
+++ src/App.js
@@ -1,55 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 // Importing components
-import Search from './components/search'
-import Results from './components/results'
-
-/* Pixabay
-User: kahagi2080@mailmink.com
-Pass: hola1234 */
+import Search from "./components/search";
+import Results from "./components/results";
 
 function App() {
-
   const [searchWord, setSearchWord] = useState(null);
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(null);
 
   useEffect(() => {
+    const apiKey = "put here your api key";
+
     if (searchWord !== null) {
-      const url = `https://pixabay.com/api/?key=15216018-e65acb1671acb3c799dd2acbc&q=${searchWord}&page=${page}`;
+      const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchWord}&page=${page}`;
       fetch(url)
-        .then(ans => ans.json())
-        .then(res => setImages(res.hits))
-      
-      const topView=document.querySelector('.jumbotron');
-      topView.scrollIntoView('auto','start');
+        .then((ans) => ans.json())
+        .then((res) => setImages(res.hits));
+
+      const topView = document.querySelector(".jumbotron");
+      topView.scrollIntoView("auto", "start");
     }
-  }, [searchWord,page]);
+  }, [searchWord, page]);
 
   return (
     <div className="container">
       <div className="jumbotron my-5">
         <h3 className="text-center mb-5">Image Search</h3>
-        <Search data={data => {
-          setSearchWord(data);
-          setPage(1);
-        }} />
+        <Search
+          data={(data) => {
+            setSearchWord(data);
+            setPage(1);
+          }}
+        />
       </div>
       <Results
         imagesResults={images}
         previousPage={() => {
           let pageNum = page;
           if (pageNum > 1) {
-            pageNum-=1;
+            pageNum -= 1;
             setPage(pageNum);
-          } else{
+          } else {
             return null;
           }
         }}
         nextPage={() => {
           let pageNum = page;
-          pageNum+=1;
+          pageNum += 1;
           setPage(pageNum);
         }}
       />
